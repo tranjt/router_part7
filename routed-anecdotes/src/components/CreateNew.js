eb391ac943d87eb62cf0e3c4c62c8c7421a39283
JTTran <tranjt80>
2020-12-01 03:44:51 +0200

    7.4 anecdotes and hooks step1

--- routed-anecdotes/src/components/CreateNew.js
+++ routed-anecdotes/src/components/CreateNew.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useField } from '../hooks'
 
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const history = useHistory()
-
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    props.setNotification(`a new anecdote ${content} have been created!`)
+    props.setNotification(`a new anecdote ${content.value} have been created!`)
     setTimeout(() => {
       props.setNotification('')
     }, 10000)
@@ -30,15 +30,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input  {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input  {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input  {...info} />
         </div>
         <button>create</button>
       </form>
